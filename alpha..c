bacedd37eee22a4638b9e8ba67a339bf2b6f0612
naveenamalliga <35651865+naveenamalliga@users.noreply.github.com>
2018-01-23 16:31:30 +0530

    Create alpha..c

---
+++ alpha..c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char alpha;
+clrscr();
+printf("enter the character");
+scanf("c",alpha);
+if(alpha=%c)
+{
+printf("alphabet";
+}
+else
+{
+printf("not alphabet");
+}
+getch();
+}
