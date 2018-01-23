d1853b6bd0b7b01a4966bf78c26d4523ed92e328
naveenamalliga <35651865+naveenamalliga@users.noreply.github.com>
2018-01-23 23:04:54 +0530

    Create print.c

---
+++ print.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a='hello',i;
+int a;
+clrscr();
+printf("times to repear");
+scanf("%d",&a);
+
+for(i=1;i<=a;i++)
+{
+printf("hello");
+}
+getch();
+}
