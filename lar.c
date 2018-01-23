182815f2aaff6bf3a39a50c33f46cae89d85b8b2
naveenamalliga <35651865+naveenamalliga@users.noreply.github.com>
2018-01-23 20:42:24 +0530

    Update lar.c

--- lar.c
+++ lar.c
@@ -3,5 +3,20 @@
 void main()
 {
 int a,b,c;
+clrscr();
 printf("A=\nB=\C=");
-scanf("%d",&a);
+scanf("%d%d%d",&a,&b,&c);
+if((a>b)||(a>c))
+{
+  printf("%d",a);
+}
+  else if(b>c||b>a)
+  {
+    printf("%d",b);
+  }
+  else
+  {
+    printf("%d",c)
+  }
+  getch();
+}
