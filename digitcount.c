2dd72058024fa38b0cec32a0197f954e48ebbb77
naveenamalliga <35651865+naveenamalliga@users.noreply.github.com>
2018-01-25 16:24:19 +0530

    Create digitcount.c

---
+++ digitcount.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[i],b;
+clrscr();
+printf("enter the number=");
+scanf("%d",&a[i]);
+for(i=0;i<=a[i];i++)
+{
+b=i
+}
+printf("no of digits=%d",b);
+getch();
+}
+
