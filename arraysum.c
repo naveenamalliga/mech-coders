1659bd73f7b41637c2b2ce6603c9bdd9fcd6f4c2
naveenamalliga <35651865+naveenamalliga@users.noreply.github.com>
2018-01-30 16:43:29 +0530

    Update arraysum.c

--- arraysum.c
+++ arraysum.c
@@ -1,1 +1,17 @@
-#include<
+#include<stdio.h>
+#include<conio.h>
+void main();
+{
+  int a[b],b,c,i,sum;
+  clrscr();
+  printf("enter the N and K numbers");
+  scanf("%d%d",&b,&c);
+  printf("enter the array of %d integers",b);
+  scanf("%d",&a);
+  for(i=0;i<=c;i++)
+  {
+      sum=a[0]+a[i];
+  }
+  printf("the sum of the first %d integers=%d",c,sum);
+  getch();
+}
