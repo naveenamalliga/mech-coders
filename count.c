b7f4e575c74f11b6affdd5c73bd97dd7d275fb12
naveenamalliga <35651865+naveenamalliga@users.noreply.github.com>
2018-01-30 15:56:31 +0530

    Create count.c

---
+++ count.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+int a,b=0;
+getch();
+printf("enter the number=");
+scanf("%d,&a);
+while(a==0)
+{
+b=a/10;
+b++
+}
+printf("the number of digits=%d",b);
+getch();
+}
