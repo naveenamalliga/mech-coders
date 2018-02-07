cf16004c75b945133eb138a30e5e6aa077f8d3ac
naveenamalliga <35651865+naveenamalliga@users.noreply.github.com>
2018-02-07 22:33:11 +0530

    Create eveninter.c

---
+++ eveninter.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ long int a,b,i;
+printf("enter the interval");
+scanf("%ld%ld",&a,&b);
+printf("even numbers between the interval");
+for(i=a+1;i<b;i++)
+{
+if((i%2)==0)
+{
+printf("%d\n",i);
+}
+}
+getch();
+}
