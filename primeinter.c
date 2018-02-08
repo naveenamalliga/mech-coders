96b260750fbcd26f5a3f0ea0438c34b23d3b14a7
naveenamalliga <35651865+naveenamalliga@users.noreply.github.com>
2018-02-08 16:25:48 +0530

    Update primeinter.c

--- primeinter.c
+++ primeinter.c
@@ -2,14 +2,13 @@
 #include<conio.h>
 void main()
 {
- long int a,b,j,i;
- int count=0;
+ long int a,b,j,i,count;
  clrscr();
 printf("enter the interval");
 scanf("%ld%ld",&a,&b);
 printf("prime numbers between the interval");
 for(j=a+1;j<b;j++)
-{
+{  count=0;
   for(i=2;i<j;i++)
   {
     if(j%i<=0)
