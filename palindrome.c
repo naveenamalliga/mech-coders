482aa9d6bc16f06b459d01b89d330d316dc13435
naveenamalliga <35651865+naveenamalliga@users.noreply.github.com>
2018-02-11 10:20:36 +0530

    Update palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -3,17 +3,17 @@
 void main ()
 {
   int a, b, c = 0, r;
-  clrscr ();
+  clrscr();
   printf ("enter the number");
-  scanf ("%d", &a);
-  b == a;
+  scanf ("%d",&a);
+  b=a;
   while (a != 0)
     {
       r = a % 10;
       c = c * 10 + r;
       a = a / 10;
     }
-  if (c == b)
+  if (c==b)
     printf ("Yes");
   else
     printf ("No");
