c70b222c065a3bee95cef275573d4b5210aa7de8
naveenamalliga <35651865+naveenamalliga@users.noreply.github.com>
2018-02-13 16:15:13 +0530

    Create numericstring.c

---
+++ numericstring.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[10];
+clrscr();
+printf(" enter the string");
+scanf("%s",&a);
+if(a=="%d")
+{
+printf("yes");
+}
+else
+{
+printf("No"0;
+}
+getch();
+}
