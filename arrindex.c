37a7273464c25494f0ae2b3a614a2340a19e211e
naveenamalliga <35651865+naveenamalliga@users.noreply.github.com>
2018-02-22 16:23:01 +0530

    Create arrindex.c

---
+++ arrindex.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b[10],i;
+clrscr();
+printf(" enter the number of elements in an array");
+for(i=0;i<a;i++)
+{
+scanf("%d",&b[i]);
+}
+printf("the index of an array");
+for(i=0;i<a;i++)
+{
+printf("\n%d",&a[i];
+}
+getch();
+}
