5ab49097c903b3fed51d697a652741c59f9c43d2
naveenamalliga <35651865+naveenamalliga@users.noreply.github.com>
2018-02-22 16:45:49 +0530

    Update arrindex.c

--- arrindex.c
+++ arrindex.c
@@ -4,7 +4,10 @@ void main()
 {
 int a,b[10],i;
 clrscr();
+printf("enter the values of an array");
+scanf("%d",&a);
 printf(" enter the number of elements in an array");
+scanf("%d",&a);
 for(i=0;i<a;i++)
 {
 scanf("%d",&b[i]);
@@ -12,7 +15,8 @@ scanf("%d",&b[i]);
 printf("the index of an array");
 for(i=0;i<a;i++)
 {
-printf("\n%d",&a[i];
+printf("\n%d\t%d",b[i],i);
 }
 getch();
 }
+
