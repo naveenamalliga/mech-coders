ff1acb31129fe411da6782bdb26749df67cabd8b
naveenamalliga <35651865+naveenamalliga@users.noreply.github.com>
2018-02-26 21:08:31 +0530

    Update hr.c

--- hr.c
+++ hr.c
@@ -1,1 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c,d,e,f,hr=0;
+clrscr();
+printf("enter the two times in hours and minutes");
+scanf("%d%d%d%d",&a,&b,&c,&d);
+  a=(a*60)+b;
+  c=(c*60)+d;
+  if(a>c)
+  {
+  e=a-c;
+  }
+  else
+  {
+  e=c-a;
+  }
+while(e>59)
+{
+e=e-60;
+hr++;
+}
+printf("%d:%d",hr,e);
+getch();
+}
 
