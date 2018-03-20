5dee87e6cd6d123ecde525553bc4a0e86322661e
naveenamalliga <35651865+naveenamalliga@users.noreply.github.com>
2018-03-20 16:29:27 +0530

    Update add.c

--- add.c
+++ add.c
@@ -1,1 +1,12 @@
-
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a,b,c;
+  clrscr();
+  printf("enter two numbers to add");
+  scanf("%d%d",&a,&b);
+  c=a+b;
+  printf("sum=%d",c);
+  getch();
+}
